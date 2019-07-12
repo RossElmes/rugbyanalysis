76131e24969b27baa92fd507c55eb8ee78f59261
Ross Elmes <rosselmes@hotmail.com>
2019-07-12 13:06:13 +0100

    Return raw dataframe and clean list of data frames for each code

--- R/load_xml.R
+++ R/load_xml.R
@@ -3,6 +3,16 @@ load_xml<- function(direc = rstudioapi::selectDirectory()){
   direc <-  rstudioapi::selectDirectory()
   files <-list.files(direc,pattern = ".xml")
   data <- future_map_dfr(paste0(direc,"/",files), ~ sportscoder::read_sportscode_xml(.))
-  invisible(data)
+
+  code_data<- data%>%
+    group_split(code)%>%
+    set_names(., nm = map(.x = ., ~glue("{first(.x$code)}")))%>%
+    map(.,function(x)remove_empty(x,"cols"))
+
+
+
+  results <- list(raw_data =data,grouped_data =code_data )
+  invisible(results)
 
 }
+
