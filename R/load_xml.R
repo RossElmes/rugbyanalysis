e2f709d1ce3196d618487059bfb935d94dce841a
Ross Elmes <rosselmes@hotmail.com>
2019-07-18 19:11:08 +0100

    Update for fix

--- R/load_xml.R
+++ R/load_xml.R
@@ -6,12 +6,14 @@ load_xml<- function(direc = rstudioapi::selectDirectory()){
 
   cols_names = c("start", "end", "code")
 
-  code_data<- data%>%
+  data<- data%>%
     select(-one_of(cols_names))%>%
     map_df(~sapply(strsplit(as.character(.), "- "), tail, 1)) %>%
     bind_cols(data[, cols_names], .) %>%
     mutate(bip.time = as.numeric(end) - as.numeric(start),
-           start = as.numeric(start), end = as.numeric(end))%>%
+           start = as.numeric(start), end = as.numeric(end))
+
+  code_data <-data%>%
     group_split(code)%>%
     set_names(., nm = map(.x = ., ~glue("{first(.x$code)}")))%>%
     map(.,function(x)remove_empty(x,"cols"))
@@ -21,4 +23,8 @@ load_xml<- function(direc = rstudioapi::selectDirectory()){
   invisible(results)
 
 }
+
+library(rugbyanalysis)
+
+rugbyanalysis::load_xml()
 
