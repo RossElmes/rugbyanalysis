45991707c8b5b9587cc127e1a957fcb1f2792f23
Ross Elmes <rosselmes@hotmail.com>
2019-07-18 18:40:53 +0100

    Change to load to add cleaning step earlier

--- R/load_xml.R
+++ R/load_xml.R
@@ -4,14 +4,20 @@ load_xml<- function(direc = rstudioapi::selectDirectory()){
   files <-list.files(direc,pattern = ".xml")
   data <- future_map_dfr(paste0(direc,"/",files), ~ sportscoder::read_sportscode_xml(.))
 
+  cols_names = c("start", "end", "code")
+
   code_data<- data%>%
+    select(-one_of(cols_names))%>%
+    map_df(~sapply(strsplit(as.character(.), "- "), tail, 1)) %>%
+    bind_cols(data[, cols_names], .) %>%
+    mutate(bip.time = as.numeric(end) - as.numeric(start),
+           start = as.numeric(start), end = as.numeric(end))%>%
     group_split(code)%>%
     set_names(., nm = map(.x = ., ~glue("{first(.x$code)}")))%>%
     map(.,function(x)remove_empty(x,"cols"))
 
+  results <- list(raw_data =data,grouped_data =code_data)
 
-
-  results <- list(raw_data =data,grouped_data =code_data )
   invisible(results)
 
 }
